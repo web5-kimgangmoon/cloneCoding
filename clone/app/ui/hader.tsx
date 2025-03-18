8211b08bb44da3f0a9807b2a5217c46eec00b301
kim gang moon <rkdans2111@naver.com>
2025-03-18 19:03:11 +0900

    feat:top header layout

--- clone/app/ui/hader.tsx
+++ clone/app/ui/hader.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export const Header = () => {
-  return <></>;
+  return (
+    <header className="fixed top-0 left-0 bg-white w-full">
+      <div className="container flex justify-between items-center h-24 m-auto px-44">
+        <div>
+          <h5 className="text-xs text-gray-800">전화상담문의</h5>
+          <h3 className="text-2xl font-bold text-indigo-600">02·479·4050</h3>
+        </div>
+        <h1 className="text-4xl font-bold">경일IT 아카데미</h1>
+        <div className="h-full relative w-[20rem]">
+          <Image
+            src={"/main_hd_btn4.png"}
+            alt="main_hd_btn4.png"
+            fill
+            objectFit="contain"
+          />
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
 };
